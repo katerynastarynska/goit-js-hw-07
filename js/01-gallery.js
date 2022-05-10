4f0ca4622c2042bae4d0659268bc80bbc93e1f22
Kateryna Starynska <katerynastarynska@gmail.com>
2022-05-10 16:39:10 -0500

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const itemsMarkUp = galleryItems.map((item) => {
+    gallery.insertAdjacentHTML('beforeEnd',
+        `<div class="gallery__item">
+            <a class="gallery__link" href="">
+                <img class="gallery__image"
+                src=${item.preview} 
+                data-source="${item.original}"
+                alt=${item.description}/>
+            </a>
+        </div>`
+    )
+}
+).join(" ");
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src=${event.target.dataset.source} width="800" height="600">`)
+    instance.show();
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            instance.close();
+        }
+    });
+}
+
